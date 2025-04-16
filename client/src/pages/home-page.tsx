7020e56021f224104e088187aa3a353cb5ae0032
Mircea Carasel <mirciuss@gmail.com>
2025-04-16 15:59:04 +0000

    style

--- client/src/pages/home-page.tsx
+++ client/src/pages/home-page.tsx
@@ -59,7 +59,7 @@ export default function HomePage() {
 
   return (
     <div className="container mx-auto px-0.5 py-1">
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-0.5">
+      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 xl:grid-cols-8 2xl:grid-cols-10 gap-1">
         {filteredProducts?.map((product) => (
           <div 
             key={product.id} 
